d2196a3bc6760369625c06b9eeb2bd961d7c96bc
James Tranovich <jtranovich@gmail.com>
2014-12-08 22:28:18 -0800

    Fixing skewer.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
