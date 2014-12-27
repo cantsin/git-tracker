59b069a6859b7a4fca2fd1ebe3f42ba753350d33
James Tranovich <jtranovich@gmail.com>
2014-12-26 16:44:11 -0800

    Treemap fixes.

--- static/js/git-treemap.js
+++ static/js/git-treemap.js
@@ -1,15 +1,18 @@
 /* jshint esnext:true */
 
-let count = $('#treemap').data('repository_count');
-let labels = $('#treemap').data('tags');
+let data = $('#treemap').data('tags');
+data = data.filter((t) => { return t.count !== 0; });
+data = data.sort((a, b) => { return b.count - a.count; });
+
+let labels = R.pluck('tag')(data);
 
 // normalize the data.
-let n = Math.max.apply(null, count) || 1;
-let data = [for (i of count) i / n];
+let n = data[0].count;
+let counts = [for (i of R.pluck('count')(data)) i / n];
 
 let boxFormatter = (coordinates, index) => {
   // we don't have any recursive indices.
-  let datapoint = data[index[0]];
+  let datapoint = counts[index[0]];
 
   let saturation = (datapoint * 0.6) + 0.4;
   let brightness = (datapoint * 0.3) + 0.2;
@@ -19,4 +22,4 @@ let boxFormatter = (coordinates, index) => {
 };
 
 let width = $('#treemap').width();
-Treemap.draw('treemap', width, 200, data, labels, {'box' : boxFormatter});
+Treemap.draw('treemap', width, 200, counts, labels, {'box' : boxFormatter});
