1cf790bb1c42fffd22e8058a64eb85f14c91baea
James Tranovich <jtranovich@gmail.com>
2015-01-06 21:10:31 -0800

    CSS/JS tweaks.

--- static/js/user.js
+++ static/js/user.js
@@ -4,6 +4,7 @@ import { bind_creation } from '/js/common';
 
 bind_creation($('#add_emails_form'));
 
+// delete user emails.
 $("#add_emails_form").find('li a').click(function() {
   let uri = $(this).attr('href');
   let result = $.getJSON(uri);
@@ -14,7 +15,7 @@ $("#add_emails_form").find('li a').click(function() {
 $("input:file").change(function(e) {
   let split_filename = $(this).val().split('\\');
   let filename = split_filename[split_filename.length - 1];
-  $(this).parent().next().remove();
+  $(this).parent().parent().next().find('label').remove();
   let html = '<label>Uploaded file: ' + filename + '</label>';
-  $(this).parent().after(html);
+  $(this).parent().parent().next().append(html);
 });
