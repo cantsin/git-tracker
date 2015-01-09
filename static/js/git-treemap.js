00c25bd6cc6d891cf5cf3048dbfd6038ab304af1
James Tranovich <jtranovich@gmail.com>
2015-01-08 20:07:04 -0800

    Better behavior for empty treemap/repository.

--- static/js/git-treemap.js
+++ static/js/git-treemap.js
@@ -6,20 +6,23 @@ data = data.sort((a, b) => { return b.count - a.count; });
 
 let labels = R.pluck('tag')(data);
 
-// normalize the data.
-let n = data[0].count;
-let counts = [for (i of R.pluck('count')(data)) i / n];
+// do we have data to work with?
+if(labels.count) {
+  // normalize the data.
+  let n = data[0].count;
+  let counts = [for (i of R.pluck('count')(data)) i / n];
 
-let boxFormatter = (coordinates, index) => {
-  // we don't have any recursive indices.
-  let datapoint = counts[index[0]];
+  let boxFormatter = (coordinates, index) => {
+    // we don't have any recursive indices.
+    let datapoint = counts[index[0]];
 
-  let saturation = (datapoint * 0.6) + 0.4;
-  let brightness = (datapoint * 0.3) + 0.2;
-  let color = 'hsb(0.6,' + saturation + ',' + brightness + ')';
+    let saturation = (datapoint * 0.6) + 0.4;
+    let brightness = (datapoint * 0.3) + 0.2;
+    let color = 'hsb(0.6,' + saturation + ',' + brightness + ')';
 
-  return {'fill' : color};
-};
+    return {'fill' : color};
+  };
 
-let width = $('#treemap').width();
-Treemap.draw('treemap', width, 200, counts, labels, {'box' : boxFormatter});
+  let width = $('#treemap').width();
+  Treemap.draw('treemap', width, 200, counts, labels, {'box' : boxFormatter});
+}
