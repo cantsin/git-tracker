727b8fb2434f87f1095eb0835bfe6f6f98333d19
James Tranovich <james@tranovi.ch>
2015-02-17 20:17:58 -0800

    Elm experiment: changing dates.

--- static/elm/Heatmap.elm
+++ static/elm/Heatmap.elm
@@ -2,9 +2,10 @@ module Heatmap where
 
 import Text
 import List
-import Date (Date, toTime, fromTime)
-import Time (Time, hour)
+import Time
+import Date (..)
 import Html (..)
+import Html.Events (..)
 import Signal
 
 port appMinDate: Float
@@ -22,11 +23,14 @@ type alias CalendarView = { minDate: Date
 -- drawing options.
 -- TODO
 
--- actions that the user can do.
+-- actions that can be applied to our model.
 type Action = PreviousCalendar | NextCalendar | Display
 
-daysToFloat: Int -> Time
-daysToFloat days = (toFloat days) * 24 * hour
+oneday: Time.Time
+oneday = 24 * Time.hour
+
+daysToFloat: Int -> Time.Time
+daysToFloat days = (toFloat days) * oneday
 
 minDate: Date -> Date -> Date
 minDate d1 d2 = if (toTime d1) < (toTime d2) then d1 else d2
@@ -38,17 +42,45 @@ update: Action -> CalendarView -> CalendarView
 update action cal =
     case action of
         PreviousCalendar ->
-            let previous = toTime cal.starting + daysToFloat cal.days in
+            let previous = toTime cal.starting - daysToFloat cal.days in
             { cal | starting <- maxDate cal.minDate (fromTime previous) }
         NextCalendar ->
-            let next = toTime cal.starting - daysToFloat cal.days in
+            let next = toTime cal.starting + daysToFloat cal.days in
             { cal | starting <- minDate cal.maxDate (fromTime next) }
 
--- TODO: print out dates
--- TODO: add < and > buttons
+-- mm/dd/YYYY
+monthToString: Month -> String
+monthToString m =
+    case m of
+      Jan -> "01"
+      Feb -> "02"
+      Mar -> "03"
+      Apr -> "04"
+      May -> "05"
+      Jun -> "06"
+      Jul -> "07"
+      Aug -> "08"
+      Sep -> "09"
+      Oct -> "10"
+      Nov -> "11"
+      Dec -> "12"
+
+printDate: Float -> Html
+printDate when =
+    let date = fromTime when
+        m = month date
+        d = day date
+        y = year date in
+    text <| monthToString m ++ "/" ++ toString d ++ "/" ++ toString y
+
 view: CalendarView -> Html
 view cal =
-    div [] <| List.repeat cal.days <| div [] []
+    let clickPrev = button [ onClick (Signal.send actionChannel PreviousCalendar) ] [ text "<" ]
+        clickNext = button [ onClick (Signal.send actionChannel NextCalendar) ] [ text ">" ]
+        header = printDate <| toTime cal.starting
+        start = toTime cal.starting
+        calendar = List.map (\n -> div [] [ printDate (start + (toFloat n) * oneday) ]) [1..cal.days] in
+    div [] <| header :: clickPrev :: clickNext :: calendar
 
 model: Signal CalendarView
 model =
