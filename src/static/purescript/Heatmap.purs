4df822a1b28da19fe17dbecaeb5a0f737018a8de
James Tranovich <james@tranovi.ch>
2015-03-20 16:00:55 -0700

    Get days of month.

--- src/static/purescript/Heatmap.purs
+++ src/static/purescript/Heatmap.purs
@@ -1,9 +1,18 @@
 module Heatmap where
 
+import Data.Date (Year(..), Month(..), day, date)
+import Data.Maybe
+import Data.Enum
 import Debug.Trace
 import Math
 
-diagonal :: Number -> Number -> Number
-diagonal w h = sqrt (w * w + h * h)
+-- calculate the days of the month
+daysOfMonth :: Year -> Month -> Maybe Number
+daysOfMonth y m =
+  day <$> date y next 0
+  where
+  next = case succ m of
+    Just(m) -> m
+    Nothing -> December
 
-main = print $ diagonal 3 4
+main = print $ daysOfMonth 2015 February
