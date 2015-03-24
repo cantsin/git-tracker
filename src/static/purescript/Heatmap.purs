e4d6bd82b8e7d44a0112559cf82188445dc35fcb
James Tranovich <james@tranovi.ch>
2015-03-23 22:27:49 -0700

    Added purescript-maps.

--- src/static/purescript/Heatmap.purs
+++ src/static/purescript/Heatmap.purs
@@ -1,10 +1,19 @@
 module Heatmap where
 
-import Data.Date (Year(..), Month(..), day, date)
+import Data.Date (Date(), Year(..), Month(..), day, date)
 import Data.Maybe
 import Data.Enum
+import Data.Map
 import Debug.Trace
 import Math
+
+data CalendarParameters = CalendarParameters { earliest :: Date
+                                             , latest :: Date
+                                             , current :: Date
+                                             , range :: Number -- number of months; must be at least one
+                                             }
+
+data Calendar = Map Date Number
 
 -- calculate the days of the month
 daysOfMonth :: Year -> Month -> Maybe Number
