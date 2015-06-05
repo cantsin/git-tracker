fae1d6a0e67c578d0fc509c447af885b5727822e
James Tranovich <jtranovich@gmail.com>
2015-06-04 17:12:23 -0700

    Remove unneeded javascript.

--- static/js/user.js
+++ static/js/user.js
@@ -11,11 +11,3 @@ $("#add_emails_form").find('li a').click(function() {
   $(this).parents('li').fadeOut();
   return false;
 });
-
-$("input:file").change(function(e) {
-  let split_filename = $(this).val().split('\\');
-  let filename = split_filename[split_filename.length - 1];
-  $(this).parent().parent().next().find('label').remove();
-  let html = '<label>Uploaded file: ' + filename + '</label>';
-  $(this).parent().parent().next().append(html);
-});
