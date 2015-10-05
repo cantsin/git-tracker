38b6c2ccc8a46935ba55a1c78c269cc5032da275
James Tranovich <jtranovich@gmail.com>
2015-10-05 07:57:06 -0700

    Removing template/static references.

--- app.py
+++ app.py
@@ -11,13 +11,11 @@ def override_loader(*args, **kwargs):
         return None
 pkgutil.get_loader = override_loader
 
-from flask import Flask, render_template, request, redirect, url_for, \
-    jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 from flask.ext.login import LoginManager, login_required, login_user, \
     logout_user, current_user
 from werkzeug import secure_filename
-from util import slugify, naturaltime, get_gravatar, \
-    url_for_redirect_back, get_redirect_target
+from util import get_gravatar
 from models import User, Tag
 from git import GitOperations, GitException
 from data import DataOperations
@@ -29,11 +27,17 @@ import os
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask('git-tracker')
-app.jinja_env.filters['slugify'] = slugify
-app.jinja_env.filters['naturaltime'] = naturaltime
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+def success(result=None):
+    if not result:
+        return jsonify({success: True})
+    return jsonify({success: True, result: result})
+
+def failure(error):
+    return jsonify({success: False, error: error})
 
 @login_manager.user_loader
 def load_user(userid):
@@ -41,35 +45,23 @@ def load_user(userid):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', error=error), 404
+    return failure('')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    error = None
-    next_target = get_redirect_target()
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(login=email).scalar()
-        if user and user.check_password(password):
-            login_user(user)
-            url = url_for_redirect_back('dashboard')
-            return redirect(url)
-        else:
-            error = 'Email and password do not match.'
-    return render_template('index.html',
-                           next=next_target,
-                           error=error)
+    email = request.form['email']
+    password = request.form['password']
+    user = User.query.filter_by(login=email).scalar()
+    if user and user.check_password(password):
+        login_user(user)
+        return success()
+    return failure('Email and password do not match.')
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return success()
 
 # helper function.
 def save_uploaded_file(user, request_file):
@@ -79,35 +71,29 @@ def save_uploaded_file(user, request_file):
     request_file.save(path)
     return path
 
-@app.route('/users/add', methods=['GET', 'POST'])
+@app.route('/users/add', methods=['POST'])
 def add_user():
-    if request.method == 'POST':
-        email = request.form['email']
-        if not '@' in email:
-            error = 'Please provide a proper email.'
-            return render_template('user_form.html', error=error)
-        password1 = request.form['password']
-        password2 = request.form['password2']
-        if password1 != password2:
-            error = 'Passwords do not match.'
-            return render_template('user_form.html', error=error)
-        public_key = request.files['public-key']
-        if not public_key.filename:
-            error = 'No public key provided.'
-            return render_template('user_form.html', error=error)
-        private_key = request.files['private-key']
-        if not private_key.filename:
-            error = 'No private key provided.'
-            return render_template('user_form.html', error=error)
-        new_user = User(email, password1)
-        new_user.avatar_image = get_gravatar(email)
-        new_user.save() # generate an id for this user
-        new_user.ssh_public_key_path = save_uploaded_file(new_user, public_key)
-        new_user.ssh_private_key_path = save_uploaded_file(new_user, private_key)
-        new_user.save()
-        new_user.add_emails(email)
-        return redirect(url_for('login'))
-    return render_template('user_form.html')
+    email = request.form['email']
+    if not '@' in email:
+        return failure('Please provide a proper email.')
+    password1 = request.form['password']
+    password2 = request.form['password2']
+    if password1 != password2:
+        return failure('Passwords do not match.')
+    public_key = request.files['public-key']
+    if not public_key.filename:
+        return failure('No public key provided.')
+    private_key = request.files['private-key']
+    if not private_key.filename:
+        return failure('No private key provided.')
+    new_user = User(email, password1)
+    new_user.avatar_image = get_gravatar(email)
+    new_user.save() # generate an id for this user
+    new_user.ssh_public_key_path = save_uploaded_file(new_user, public_key)
+    new_user.ssh_private_key_path = save_uploaded_file(new_user, private_key)
+    new_user.save()
+    new_user.add_emails(email)
+    return success()
 
 @app.route('/users/keys', methods=['POST'])
 @login_required
@@ -121,10 +107,9 @@ def update_keys():
         if private_key.filename:
             current_user.ssh_private_key_path = save_uploaded_file(current_user, private_key)
             current_user.save()
-        url = url_for_redirect_back('dashboard')
-        return redirect(url)
+        return success()
     except KeyError:
-        return jsonify(error='Please fill out all fields.')
+        return failure('Please fill out all fields.')
 
 @app.route('/users/email/add', methods=['POST'])
 @login_required
@@ -132,14 +117,13 @@ def add_user_email():
     try:
         email = request.form['email']
         if not '@' in email:
-            return jsonify(error='Please provide a proper email.')
+            return failure('Please provide a proper email.')
         if current_user.emails.filter_by(email=email).scalar():
-            return jsonify(error='Current email already exists.')
+            return failure('Current email already exists.')
         current_user.add_emails(email)
-        url = url_for_redirect_back('dashboard')
-        return jsonify(success=url)
+        return success()
     except IndexError:
-        return jsonify(error='Please fill out all fields.')
+        return failure('Please fill out all fields.')
 
 @app.route('/users/email/<useremail_id>/delete', methods=['GET'])
 @login_required
@@ -147,7 +131,7 @@ def delete_user_email(useremail_id):
     ue = current_user.emails.filter_by(id=useremail_id).first_or_404()
     ue.delete()
     url = url_for('dashboard')
-    return jsonify(success=url)
+    return success()
 
 @app.route('/repository/<name>')
 @login_required
@@ -156,13 +140,13 @@ def view_repository(name):
     identifier = repository.get_shorthand_of_branch('master')
     sha1 = repository.get_sha1_of_branch('master')
     tags = current_user.tags.order_by('name')
-    kwargs = {'repository': repository,
-              'current_selection': repository.name,
-              'git_identifier': identifier,
-              'git_sha1': sha1,
-              'tags': tags,
-              'selection': 'repositories'}
-    return render_template('view_repository.html', **kwargs)
+    result= {'repository': repository,
+             'current_selection': repository.name,
+             'git_identifier': identifier,
+             'git_sha1': sha1,
+             'tags': tags,
+             'selection': 'repositories'}
+    return success(result=result)
 
 @app.route('/repositories/<name>/activity/', methods=['GET'])
 @login_required
@@ -170,7 +154,8 @@ def repository_activity(name):
     repository = current_user.repositories.filter_by(name=name).first_or_404()
     start = int(request.args.get('start')) or repository.get_first_updated()
     end = int(request.args.get('end')) or repository.get_last_updated()
-    return jsonify({'result': repository.histogram(start, end)})
+    result = repository.histogram(start, end)
+    return success(result=result)
 
 @app.route('/repositories/<name>/delete', methods=['GET'])
 @login_required
@@ -178,7 +163,7 @@ def delete_repository(name):
     repository = current_user.repositories.filter_by(name=name).first_or_404()
     repository.clear_tags()
     repository.delete()
-    return redirect(url_for('dashboard'))
+    return success()
 
 @app.route('/repositories/<name>/refresh', methods=['GET'])
 @login_required
@@ -187,8 +172,7 @@ def refresh_repository(name):
     repository.refresh()
     repository.update_commit_info()
     repository.save()
-    url = url_for('view_repository', name=repository.name)
-    return jsonify(success=url)
+    return success()
 
 @app.route('/repositories/add', methods=['POST'])
 @login_required
@@ -196,16 +180,15 @@ def add_repository():
     try:
         location = request.form['location']
         if current_user.repositories.filter_by(location=location).scalar():
-            return jsonify(error='Given repository already exists.')
+            return failure('Given repository already exists.')
         repository = GitOperations.create_repository(current_user, location)
         repository.save()
         repository.update_commit_info()
-        url = url_for('view_repository', name=repository.name)
-        return jsonify(success=url)
+        return success()
     except GitException as ge:
-        return jsonify(error=ge.args)
+        return failure(ge.args)
     except IndexError:
-        return jsonify(error='Location is invalid.')
+        return failure('Location is invalid.')
 
 @app.route('/repositories/dump', methods=['GET'])
 @login_required
@@ -225,7 +208,7 @@ def dump_repositories():
 def load_repositories():
     csv_file = request.files['bulk-upload']
     if not csv_file.filename:
-        return redirect('dashboard')
+        return failure('No file uploaded.')
     csv_path = save_uploaded_file(current_user, csv_file)
     fake_csv = csv.reader(open(csv_path, 'r+'))
     for line in fake_csv:
@@ -247,7 +230,7 @@ def load_repositories():
             tag = has_tag.first() if has_tag.all() else Tag(current_user, tag_name).save()
             if repository.tags.count(tag) == 0:
                 repository.tags.append(tag)
-    return redirect('dashboard')
+    return success()
 
 @app.route('/repository/<repository_name>/tags/apply', methods=['POST'])
 @login_required
@@ -260,8 +243,7 @@ def apply_tags(repository_name):
         tag = current_user.tags.filter_by(slug=tag_name[6:]).first_or_404()
         repository.tags.append(tag)
     repository.save()
-    url = url_for('view_repository', name=repository_name)
-    return jsonify(success=url)
+    return success()
 
 @app.route('/tag/<slug>')
 @login_required
@@ -269,12 +251,12 @@ def view_tag(slug):
     tag = current_user.tags.filter_by(slug=slug).first_or_404()
     first_updated = DataOperations.get_first_updated(tag.repositories)
     last_updated = DataOperations.get_last_updated(tag.repositories)
-    kwargs = {'tag': tag,
+    result = {'tag': tag,
               'current_selection': tag.name,
               'first_updated': first_updated,
               'last_updated': last_updated,
               'selection': 'tags'}
-    return render_template('view_tag.html', **kwargs)
+    return success(result=result)
 
 @app.route('/tags/<slug>/activity/', methods=['GET'])
 @login_required
@@ -285,14 +267,14 @@ def tag_activity(slug):
     start = int(request.args.get('start')) or first_updated
     end = int(request.args.get('end')) or last_updated
     result = DataOperations.histogram(tag.repositories, start, end)
-    return jsonify({'result': result})
+    return success(result=result)
 
 @app.route('/tags/<slug>/delete', methods=['GET'])
 @login_required
 def delete_tag(slug):
     tag = current_user.tags.filter_by(slug=slug).first_or_404()
     tag.delete()
-    return redirect(url_for('dashboard'))
+    return success()
 
 @app.route('/tags/add', methods=['POST'])
 @login_required
@@ -300,16 +282,15 @@ def add_tag():
     try:
         name = request.form['name'].strip()
         if name == '':
-            return jsonify(error='Tag cannot be blank.')
+            return failure('Tag cannot be blank.')
         if current_user.tags.filter_by(name=name).scalar():
-            return jsonify(error='Given tag name already exists.')
+            return failure('Given tag name already exists.')
         if current_user.tags.filter_by(slug=slugify(name)).scalar():
-            return jsonify(error='Given tag slug already exists.')
+            return failure('Given tag slug already exists.')
         tag = Tag(current_user, name).save()
-        url = url_for('view_tag', slug=tag.slug)
-        return jsonify(success=url)
+        return success()
     except IndexError:
-        return jsonify(error='Name field is invalid.')
+        return failure('Name field is invalid.')
 
 @app.route('/dashboard/activity')
 @login_required
@@ -319,21 +300,17 @@ def all_activity():
     start = int(request.args.get('start')) or first_updated
     end = int(request.args.get('end')) or last_updated
     result = DataOperations.histogram(current_user.repositories, start, end)
-    return jsonify({'result': result})
+    return success(result=result)
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
     first_updated = DataOperations.get_first_updated(current_user.repositories)
     last_updated = DataOperations.get_last_updated(current_user.repositories)
-    kwargs = {'first_updated': first_updated,
+    result = {'first_updated': first_updated,
               'last_updated': last_updated,
               'selection': 'repositories'}
-    return render_template('dashboard.html', **kwargs)
-
-@app.route('/<path:path>')
-def static_proxy(path):
-    return app.send_static_file(path)
+    return success(result=result)
 
 if __name__ == '__main__':
     try:
--- util.py
+++ util.py
@@ -9,22 +9,6 @@ import hashlib
 import humanize
 import re
 
-_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
-
-def slugify(text, delim=b'-'):
-    """Generates an ASCII-only slug."""
-    result = []
-    for word in _punct_re.split(text.lower()):
-        word = normalize('NFKD', word).encode('ascii', 'ignore')
-        if word:
-            result.append(word)
-    return delim.join(result).decode("utf-8", "strict")
-
-def naturaltime(ue):
-    if isinstance(ue, int):
-        return humanize.naturaltime(datetime.fromtimestamp(ue))
-    return humanize.naturaltime(ue)
-
 def get_gravatar(email):
     size = 69
     base_url = 'http://www.gravatar.com/avatar/'
@@ -38,20 +22,3 @@ def is_safe_url(target):
     test_url = urllib.parse.urlparse(parsed_url)
     return test_url.scheme in ('http', 'https') and \
            ref_url.netloc == test_url.netloc
-
-def url_for_redirect_back(endpoint, **values):
-    target = request.form['next']
-    if not target or not is_safe_url(target):
-        target = url_for(endpoint, **values)
-    return target
-
-def get_redirect_target():
-    for target in request.values.get('next'), request.referrer:
-        # when adding a new user, don't end up back to the new user
-        # page when we successfully login!
-        if request.referrer and request.referrer.endswith(url_for('add_user')):
-            continue
-        if not target:
-            continue
-        if is_safe_url(target):
-            return target
